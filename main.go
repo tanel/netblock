d01b9e414ca0d732047e44c7d4f26bdcfe411fca
Tanel Lebedev <tanel.lebedev@gmail.com>
2018-02-25 20:13:26 +0200

    Refactor

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
-	"path/filepath"
+	"strings"
 )
 
 func main() {
@@ -13,15 +15,24 @@ func main() {
 	}
 }
 
+const filename = "/etc/hosts"
+
 func run() error {
-	var h hosts
-	if err := h.readFile(filepath.Join("/", "etc", "hosts")); err != nil {
+	lines, err := readFile(filename)
+	if err != nil {
 		return err
 	}
 
-	cmd, site := parse(os.Args[1:])
-	if err := h.apply(cmd, site); err != nil {
+	cmd, site := parseArgs(os.Args[1:])
+	result, err := apply(lines, cmd, site)
+	if err != nil {
 		return err
+	}
+
+	if cmd != cmdList {
+		if err := writeFile(filename, result); err != nil {
+			return err
+		}
 	}
 
 	return nil
@@ -33,7 +44,7 @@ const (
 	cmdList   = "list"
 )
 
-func parse(args []string) (cmd, site string) {
+func parseArgs(args []string) (cmd, site string) {
 	switch len(args) {
 	case 1:
 		return args[0], ""
@@ -42,4 +53,141 @@ func parse(args []string) (cmd, site string) {
 	default:
 		return "", ""
 	}
+}
+
+const (
+	sectionBegin = "# BEGIN section for netblock sites"
+	sectionEnd   = "# END section for netblock sites"
+)
+
+type line struct {
+	content       string
+	isBlockedSite bool
+}
+
+func host(l line) (string, error) {
+	cols := strings.Split(l.content, "\t")
+	if len(cols) != 2 {
+		return "", fmt.Errorf("2 cols expected, got %d (%s)", len(cols), l.content)
+	}
+
+	return cols[1], nil
+}
+
+func readFile(filename string) ([]line, error) {
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	return parseLines(strings.Split(string(b), "\n")), nil
+}
+
+func writeFile(filename string, lines []line) error {
+	// FIXME:
+	return nil
+}
+
+func parseLines(all []string) []line {
+	var result []line
+	begin, end := false, false
+	for _, s := range all {
+		var l line
+		l.content = s
+
+		switch s {
+		case sectionBegin:
+			begin = true
+			end = false
+		case sectionEnd:
+			end = true
+		default:
+			l.isBlockedSite = begin && !end
+		}
+
+		result = append(result, l)
+	}
+
+	return result
+}
+
+func apply(lines []line, cmd, site string) ([]line, error) {
+	switch cmd {
+	case cmdAdd:
+		return add(lines, site)
+	case cmdRemove:
+		return remove(lines, site)
+	case cmdList:
+		return list(lines)
+	default:
+		return nil, errors.New("please specify a command: list, add, remove")
+	}
+}
+
+func add(lines []line, site string) ([]line, error) {
+	if site == "" {
+		return nil, errors.New("please specify a site to add")
+	}
+
+	lineToAdd := line{
+		content:       "127.0.0.1\t" + site,
+		isBlockedSite: true,
+	}
+
+	var result []line
+	added := false
+	for _, l := range lines {
+		if l.isBlockedSite && !added {
+			result = append(result, lineToAdd)
+			added = true
+		}
+
+		result = append(result, l)
+	}
+
+	if !added {
+		result = append(result, line{
+			content:       sectionBegin,
+			isBlockedSite: false,
+		})
+		result = append(result, lineToAdd)
+		result = append(result, line{
+			content:       sectionEnd,
+			isBlockedSite: false,
+		})
+	}
+
+	return result, nil
+}
+
+func remove(lines []line, site string) ([]line, error) {
+	if site == "" {
+		return nil, errors.New("please specify a site to remove")
+	}
+
+	var result []line
+	removed := false
+	for _, l := range lines {
+		if l.isBlockedSite && strings.Contains(l.content, site) {
+			removed = true
+		} else {
+			result = append(result, l)
+		}
+	}
+
+	if !removed {
+		return nil, fmt.Errorf("%s not found", site)
+	}
+
+	return result, nil
+}
+
+func list(lines []line) ([]line, error) {
+	for _, l := range lines {
+		if l.isBlockedSite {
+			fmt.Println(host(l))
+		}
+	}
+
+	return lines, nil
 }
--- main_test.go
+++ main_test.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"path/filepath"
 	"testing"
 )
 
-func Test_parse(t *testing.T) {
+func Test_parseArgs(t *testing.T) {
 	var examples = []struct {
 		args         []string
 		expectedCmd  string
@@ -33,7 +34,7 @@ func Test_parse(t *testing.T) {
 	}
 
 	for _, example := range examples {
-		cmd, site := parse(example.args)
+		cmd, site := parseArgs(example.args)
 		if cmd != example.expectedCmd {
 			t.Errorf("cmd expected %s but got %s", example.expectedCmd, cmd)
 		}
@@ -41,5 +42,144 @@ func Test_parse(t *testing.T) {
 		if site != example.expectedSite {
 			t.Errorf("site expected %s but got %s", example.expectedSite, site)
 		}
+	}
+}
+
+func Test_apply(t *testing.T) {
+	var lines []line
+	result, err := apply(lines, "", "")
+	if err == nil {
+		t.Error("error expected")
+	}
+
+	if result != nil {
+		t.Error("nil result expected")
+	}
+}
+
+func Test_apply_add_site(t *testing.T) {
+	var lines []line
+	result, err := apply(lines, cmdAdd, "test.com")
+	if err != nil {
+		t.Error(err)
+	}
+
+	found := false
+	for _, site := range result {
+		if site.content == "127.0.0.1\ttest.com" {
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		t.Error("site not added")
+	}
+}
+
+func Test_apply_remove_site_nonexistant(t *testing.T) {
+	var lines []line
+	result, err := apply(lines, cmdRemove, "test.com")
+	if err == nil {
+		t.Error("error expected")
+	}
+
+	if result != nil {
+		t.Error("nil result expected")
+	}
+}
+
+func Test_apply_remove_site(t *testing.T) {
+	var lines []line
+	lines = append(lines, line{
+		content:       "test.com",
+		isBlockedSite: true,
+	})
+	result, err := apply(lines, cmdRemove, "test.com")
+	if err != nil {
+		t.Error(err)
+	}
+
+	found := false
+	for _, site := range result {
+		if site.content == "test.com" {
+			found = true
+			break
+		}
+	}
+
+	if found {
+		t.Error("site not remove")
+	}
+
+}
+
+func Test_apply_list(t *testing.T) {
+	var lines []line
+	lines = append(lines, line{
+		content: "test.com",
+	})
+	result, err := apply(lines, cmdList, "")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(result) != len(lines) {
+		t.Error("output should be same as input")
+	}
+
+	if result[0].content != lines[0].content {
+		t.Error("output should be same as input")
+	}
+
+	if result[0].isBlockedSite != lines[0].isBlockedSite {
+		t.Error("output should be same as input")
+	}
+}
+
+func Test_readFile_nosites(t *testing.T) {
+	result, err := readFile(filepath.Join("testdata", "no-sites"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(result) != 10 {
+		t.Errorf("10 lines expected, got %d", len(result))
+	}
+
+	for _, l := range result {
+		if l.isBlockedSite {
+			t.Error("no sites expected")
+		}
+	}
+}
+
+func Test_readFile(t *testing.T) {
+	result, err := readFile(filepath.Join("testdata", "2-sites"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(result) != 15 {
+		t.Errorf("2 lines expected, got %d", len(result))
+	}
+
+	var sites []string
+	for _, l := range result {
+		if l.isBlockedSite {
+			sites = append(sites, l.content)
+		}
+	}
+
+	if len(sites) != 2 {
+		t.Errorf("2 sites expected, got %d", len(sites))
+	}
+
+	if sites[0] != "127.0.0.1\ttest.com" {
+		t.Errorf("unexpected site %s", sites[0])
+	}
+
+	if sites[1] != "127.0.0.1\twww.test.com" {
+		t.Errorf("unexpected site %s", sites[1])
 	}
 }
